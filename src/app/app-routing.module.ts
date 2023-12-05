936b54525bd64671dcb36f347a91176c689d4549
eas-21 <easwariswathi@gmail.com>
2023-12-05 18:15:12 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './core/home/home.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+const routes: Routes = [{
+  path: '', component :AdminLayoutComponent,
+  children: [
+    {
+    path: '', component:HomeComponent,
+    loadChildren: ()=> import('../app/core/home/home.module').then(m => m.HomeModule)
+  }]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
